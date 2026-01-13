c505b5aeb7add5fa87697a216891c88bcbfda871
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2026-01-13 21:19:50 +0300

    🔧 Integrate project-wide text search
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- lisp/project-manager.el
+++ lisp/project-manager.el
@@ -4,16 +4,19 @@
 
 ;;; Code:
 
-(cl-defmethod project-root ((project (head local)))
-  "Return root directory of current PROJECT."
-  (cdr project))
-
 (defun my/project-try-local (dir)
   "Determine if DIR is a non-VC project.
 DIR must include a file .project.
 Inspired by: https://christiantietze.de/posts/2022/03/mark-local-project.el-directories/"
   (if-let* ((root (locate-dominating-file dir ".project")))
       (cons 'local root)))
+
+(defun my/consult-project-ripgrep-at-point ()
+  "Search text at point with consul in the current project."
+  (interactive)
+  (consult-ripgrep
+   (project-root (project-current))
+   (thing-at-point 'symbol)))
 
 
 ;; 📦 PROJECT
@@ -23,7 +26,16 @@ Inspired by: https://christiantietze.de/posts/2022/03/mark-local-project.el-dire
   ;; Switch project in the same way as projectile does.
   (setopt project-switch-commands 'project-find-file)
 
-  (add-to-list 'project-find-functions #'my/project-try-local))
+  ;; Tell consul how to identify root of a project.
+  (setopt consult-project-root-function #'project-root)
+
+  (add-to-list 'project-find-functions #'my/project-try-local)
+
+  :bind
+  (:map project-prefix-map
+        ("f" . consult-find)
+        ("s" . consult-ripgrep)
+        ("." . my/consult-project-ripgrep-at-point)))
 
 
 (provide 'project-manager)
