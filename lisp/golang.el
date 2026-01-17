448f621666a3bb4c40a15b4e42b03cc4622244f2
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2026-01-17 13:56:15 +0300

    🔧 (go) Enable some inlay-hints
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- lisp/golang.el
+++ lisp/golang.el
@@ -14,9 +14,18 @@
  (:features)
  (:format (format-all--buffer-easy executable "fmt" "--stdin")))
 
-;; 📦 GO-TS
+;; 📦 GO-TS-MODE
 ;; Tree-sitter integration for Golang.
-(use-package go-ts
+(use-package go-ts-mode
+  :config
+  ;; Regarding settings see:
+  ;; Eglot: https://www.gnu.org/software/emacs/manual/html_node/eglot/User_002dspecific-configuration.html
+  ;; Gopls: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs
+                 `(go-ts-mode . ("gopls" :initializationOptions
+                                 (:hints (:constantValues t :compositeLiteralFields t))))))
+
   :hook
   ((go-ts-mode . eglot-ensure)
    (go-ts-mode . subword-mode)))
