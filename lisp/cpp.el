2a5938f0dc49b37711b78fefcacbb7a545725e9c
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2026-07-24 09:17:58 +0300

    🔧 (cpp) Add clangd settings
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- lisp/cpp.el
+++ lisp/cpp.el
@@ -7,6 +7,21 @@
 ;; 📦 C++-TS-MODE
 ;; Tree-sitter integration for C++.
 (use-package c++-ts-mode
+  :config
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs
+                 '(c++-ts-mode
+                   . ("clangd"
+                      "-j=4"
+                      "--log=error"
+                      "--clang-tidy"
+                      "--all-scopes-completion"
+                      "--completion-style=detailed"
+                      "--background-index"
+                      "--pch-storage=memory"
+                      "--header-insertion=never"
+                      "--header-insertion-decorators=0"))))
+
   :hook
   ((c++-ts-mode . eglot-ensure)))
 
