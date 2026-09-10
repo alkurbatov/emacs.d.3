2e441680a700286216446213116e033c9a95bb78
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2026-09-10 19:00:24 +0300

    🔧 (completion) Add more useful tweaks
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- lisp/text-completion.el
+++ lisp/text-completion.el
@@ -15,11 +15,33 @@
                                         analyze-text-conversion
                                         completion-preview-insert-word))
 
+  ;; Reduce visual noise by removing redundant help messages.
+  (setopt completion-show-help nil
+          completion-show-inline-help nil)
+
   (setopt completion-preview-minimum-symbol-length 2
           completion-preview-idle-delay 0.3
           completion-preview-ignore-case t
           read-file-name-completion-ignore-case t
           completion-preview-sort-function #'identity)
+
+  ;; Do not use rows and columns for completions: a single vertical
+  ;; list is easier to follow.
+  (setopt completions-format 'one-column)
+
+  ;; Put an upper limit to the Completions window, so that it does not
+  ;; disorient me.
+  (setopt completions-max-height 12)
+
+  ;; Rely on previous inputs to surface candidates towards the top of the list.
+  (setopt completions-sort 'historical)
+
+  ;; Never switch to the Completions buffer when I type TAB, because I
+  ;; want to select candidates while the minibuffer is still in focus,
+  ;; per `minibuffer-visible-completions'.  This has the advantage of
+  ;; auto-updating the completions as I type.
+  (setq completion-auto-select nil
+        minibuffer-visible-completions t)
 
   :hook
   (after-init . global-completion-preview-mode)
